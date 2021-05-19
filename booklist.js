ce5712069e2c0be06f5d48c16d7878c9b0b165ee
meihui-220 <meihui@mcinfotech.biz>
2021-05-19 09:05:35 +0800

    click and jump to new router

--- booklist.js
+++ booklist.js
@@ -1,41 +1,49 @@
 var BookList = {
-    data() {
-      return {
-        info: null,
-        currentId: null
-      };
-    },
-    mounted() {
-      axios
-        .get("https://www.abibliadigital.com.br/api/verses/nvi/gn/1", {
-          headers: {
-            Authorization: "apiKey"
-          }
-        })
-        .then((response) => (this.info = response.data));
-    },
-    template: `
-    <div class="bookList">
-        <h3>Book List</h3>
-        <hr>
-        <div class="row">
-          <div class="col-md-2">
-            <ol>
-              <li v-for="data in info">
-              <a href="#" @click="loadbookData(data)">Book Name: {{data.name}}</a></li>
-            </ol>
-          </div>
-          <div class="col-md-10">
-            <div id="item-description">{{this.currentId}}</div>
-          </div>
+  props: ["id"],
+  data() {
+    return {
+      info: null,
+      currentId: null
+    };
+  },
+  mounted() {
+    axios
+      .get("https://www.abibliadigital.com.br/api/books", {
+        headers: {
+          Authorization: "apiKey"
+        }
+      })
+      .then((response) => (this.info = response.data));
+  },
+  template: `
+  <div class="bookList">
+      <h3>Book List</h3>
+      <hr>
+      <div class="row">
+        <div class="col-md-2">
+          <ol>
+            <li v-for="data in info">
+              <router-link to="Verses">Book: {{data.name}}</router-link>
+          </ol>
         </div>
-    </div>
-  `,
-    methods: {
-      loadbookData(id) {
-        this.currentId = id.name;
-        console.log(id);
-      }
-    },
-  };
-  
+ 
+      </div>
+  </div>
+`,
+  methods: {
+    loadbookData(id) {
+      // axios
+      // .get("https://www.abibliadigital.com.br/api/verses/nvi/gn/1", {
+      //   headers: {
+      //     Authorization: "apiKey"
+      //   }
+      // })
+      // .then((response) => (this.info = response.data));
+      this.currentId = id.name;
+      console.log(id);
+    }
+  }
+  // components: {
+  //   Verses
+  // }
+};
--- verses.js
+++ verses.js
@@ -1,19 +1,27 @@
 var Verses = {
-    props: {
-      bookID: {
-        type: Number,
-        required: true
-      },
-    },
-    template: `
-      <div>
-        <p>Book ID: {{bookID}} <br>
-        <span v-if="bookData">
-          Book Name: {{bookData.name}} <br> Book Author: {{bookData.author}}
-        </span>
-        </p>
-      </div>
-    `,
-    methods: {}
-  };
-  
+  prop: {},
+  data() {
+    return {
+      info: null,
+      currentId: null
+    };
+  },
+  mounted() {
+    axios
+      .get("https://www.abibliadigital.com.br/api/verses/nvi/gn/1", {
+        headers: {
+          Authorization: "apiKey"
+        }
+      })
+      .then((response) => (this.info = response.data.verses));
+  },
+  template: `
+  <div class="bookList">
+  <h3>Verses</h3>
+  <hr>
+    <li v-for="(data, index) in info">Number {{index+1}}: {{data.text}}
+</div>
+  `,
+  methods: {
+  }
+};
