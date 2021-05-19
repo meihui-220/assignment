e162566db7ff0e29c3e9a0f19fa586e6fbb8445a
meihui-220 <meihui@mcinfotech.biz>
2021-05-19 17:56:46 +0800

    click button and jump to next page then display random data

--- booklist.js
+++ booklist.js
@@ -3,44 +3,42 @@ var BookList = {
   data() {
     return {
       info: null,
-      currentId: null
+      currentId: null,
+      id: null
     };
   },
   mounted() {
     axios
-      .get("https://www.abibliadigital.com.br/api/books", {
+      .get("https://www.abibliadigital.com.br/api/verses/nvi/gn/1", {
         headers: {
           Authorization: "apiKey"
         }
       })
       .then((response) => (this.info = response.data));
+    this.loadbookData();
   },
   template: `
   <div class="bookList">
-      <h3>Book List</h3>
       <hr>
-      <div class="row">
-        <div class="col-md-2">
-          <ol>
-            <li v-for="data in info">
-              <router-link to="Verses">Book: {{data.name}}</router-link>
-          </ol>
-        </div>
- 
-      </div>
+      <button @click="goToHome()">Click me to generate random data!!</button>
   </div>
 `,
   methods: {
-    loadbookData(id) {
-      // axios
-      // .get("https://www.abibliadigital.com.br/api/verses/nvi/gn/1", {
-      //   headers: {
-      //     Authorization: "apiKey"
-      //   }
-      // })
-      // .then((response) => (this.info = response.data));
-      this.currentId = id.name;
-      console.log(id);
+    goToHome() {
+      this.$router.push("/Verses");
+    },
+    async loadbookData() {
+      let i = await axios.get(
+        "https://www.abibliadigital.com.br/api/verses/nvi/gn/1",
+        {
+          headers: {
+            Authorization: "apiKey"
+          }
+        }
+      );
+      this.id = i.data;
+      this.$emit("id", this.id);
+      console.log("....", this.id);
     }
   }
   // components: {
--- verses.js
+++ verses.js
@@ -1,27 +1,82 @@
 var Verses = {
-  prop: {},
+  prop: {
+    example: Object
+  },
   data() {
     return {
       info: null,
-      currentId: null
+      currentId: [],
+      id: null,
+      bookData: []
     };
   },
+  // created() {
+  //   this.id = this.$route.params.id;
+  //   console.log("iddddd", this.id);
+  // },
   mounted() {
-    axios
-      .get("https://www.abibliadigital.com.br/api/verses/nvi/gn/1", {
-        headers: {
-          Authorization: "apiKey"
-        }
-      })
-      .then((response) => (this.info = response.data.verses));
+    this.getData();
+    console.log("hi", this.currentId);
   },
   template: `
   <div class="bookList">
+  <h2>Book Name: {{bookData.name}}</h2>
   <h3>Verses</h3>
-  <hr>
     <li v-for="(data, index) in info">Number {{index+1}}: {{data.text}}
 </div>
   `,
   methods: {
+    async getData() {
+      console.log("hiiiii", this.id);
+      // versions api
+      let i = [];
+      i = await axios.get("https://www.abibliadigital.com.br/api/versions", {
+        headers: {
+          Authorization: "apiKey"
+        }
+      });
+      this.currentId = i.data[Math.floor(Math.random() * i.data.length)];
+
+      // book Api
+      // let bookData = null;
+      let j = [];
+      j = await axios.get("https://www.abibliadigital.com.br/api/books", {
+        headers: {
+          Authorization: "apiKey"
+        }
+      });
+      this.bookData = j.data[Math.floor(Math.random() * j.data.length)];
+      console.log(this.bookData);
+      // get verses api
+      let version = this.currentId.version;
+      let abbrev = "/" + this.bookData.abbrev.en;
+      let chapter = "/" + this.bookData.chapters;
+
+      // let number = "";
+      let versesUrl =
+        "https://www.abibliadigital.com.br/api/verses/" +
+        version +
+        abbrev +
+        chapter;
+      console.log("url", versesUrl);
+      axios
+        .get(versesUrl, {
+          headers: {
+            Authorization: "apiKey"
+          }
+        })
+        .then((response) => (this.info = response.data.verses));
+    }
+    // loadbookData(id) {
+    // axios
+    //   .get("https://www.abibliadigital.com.br/api/verses/nvi/gn/1", {
+    //     headers: {
+    //       Authorization: "apiKey"
+    //     }
+    //   })
+    //   .then((response) => (this.info = response.data));
+    // this.currentId = id.name;
+    // console.log(id);
+    // }
   }
 };
